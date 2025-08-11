53f01cdd7575052d389a4d2592be4ff1efeca43f
StevenJiajunli <1169736896@qq.com>
2025-08-11 13:55:44 +0800

    me_lollipop_plot.R

--- R/me_lollipop_plot.R
+++ R/me_lollipop_plot.R
@@ -12,30 +12,30 @@ me_lollipop_plot <- function(input = meta) {
   library(ggplot2)
   library(forcats)
 
-  # 预处理数据
+  # Preprocess data
   input <- data.frame(
     id = input$id,
     NES = input$NES,
     pvalue = input$pvalue
   )
 
-  # 筛选 pvalue < 0.05
+  # Filter for pvalue < 0.05
   input <- subset(input, pvalue < 0.05)
 
-  # 转换 pvalue 为 -log10
+  # Transform pvalue to -log10
   input$pvalue <- -log10(input$pvalue)
 
-  # 按 NES 排序，并保持因子顺序
+  # Sort by NES and preserve factor order
   input <- input[order(input$NES),]
   input$id <- fct_inorder(input$id)
 
-  # 开始画图
+  # Plot lollipop chart
   ggplot(input, aes(x = id, y = NES, size = pvalue)) +
     geom_segment(aes(x = id, xend = id, y = 0, yend = NES),
                  linewidth = 0.75, color = "grey90") +
     geom_point(aes(color = NES)) +
     geom_hline(yintercept = 0, color = "grey20") +
-    coord_flip() +  # 翻转坐标轴
+    coord_flip() +  # Flip coordinates
     scale_color_gradient2(low = "#0070b2", high = "#da1735", mid = "white", midpoint = 0) +
     theme_bw() +
     theme(panel.grid = element_blank(),
